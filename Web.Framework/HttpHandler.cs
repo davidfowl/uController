284e8b36323f7311f09c81f240be4fbb980e2a75
David Fowler <davidfowl@gmail.com>
2017-08-30 00:58:17 -0700

    Deleted EmptyResult

--- Web.Framework/HttpHandler.cs
+++ Web.Framework/HttpHandler.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace Web.Framework
 {
     public abstract partial class HttpHandler
     {
         public Result Execute(RequestDelegate requestDelegate) => new InlineResult(requestDelegate);
-        public Result Empty() => new EmptyResult();
+        public Result Empty() => Execute(_ => Task.CompletedTask);
         public Result BadRequest() => Status(StatusCodes.Status400BadRequest);
         public Result NotFound() => Status(StatusCodes.Status404NotFound);
         public Result Ok() => Status(StatusCodes.Status200OK);
