49cf65369724d7a80a9f55e9f8581e48b23ce67b
David Fowler <davidfowl@gmail.com>
2020-11-25 15:25:53 -0400

    Small tweaks - Removed IHttpRequestReader and IHttpResponseWriter and Json* versions - Remove EmptyResult - Rename Result  to IResult

--- src/uController/ObjectResult.cs
+++ src/uController/ObjectResult.cs
@@ -1,13 +1,10 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace uController
 {
     public class ObjectResult : IResult
     {
-        private static readonly JsonResponseWriter _writer = new JsonResponseWriter();
-
         public object Value { get; }
 
         public ObjectResult(object value)
@@ -17,9 +14,7 @@ namespace uController
 
         public Task ExecuteAsync(HttpContext httpContext)
         {
-            var responseFormatter = httpContext.RequestServices.GetService<IHttpResponseWriter>() ?? _writer;
-
-            return responseFormatter.WriteAsync(httpContext, Value);
+            return httpContext.Response.WriteAsJsonAsync(Value);
         }
     }
 }
